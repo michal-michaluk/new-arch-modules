4e5265429cb4109c6a0346176dbb3986e972c95c
Michal Michaluk <michal.michaluk@bottega.com.pl>
2022-06-28 10:02:07 +0200

    encapsulation 'alien' data in abstraction

---
+++ production-impl/src/main/java/shortages/ProductionOutputs.java
@@ -0,0 +1,29 @@
+package shortages;
+
+import entities.ProductionEntity;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class ProductionOutputs {
+    private final Map<LocalDate, List<ProductionEntity>> outputs;
+
+    public ProductionOutputs(List<ProductionEntity> productions) {
+        Map<LocalDate, List<ProductionEntity>> outputs = new HashMap<>();
+        for (ProductionEntity production : productions) {
+            if (!outputs.containsKey(production.getStart().toLocalDate())) {
+                outputs.put(production.getStart().toLocalDate(), new ArrayList<>());
+            }
+            outputs.get(production.getStart().toLocalDate()).add(production);
+        }
+        this.outputs = Collections.unmodifiableMap(outputs);
+    }
+
+    public long getOutputs(LocalDate day) {
+        long output = 0;
+        for (ProductionEntity production : outputs.get(day)) {
+            output += production.getOutput();
+        }
+        return output;
+    }
+}
