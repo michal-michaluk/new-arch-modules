e1f36d93dfa8062dbb30cf62c574624206e339f2
Michal Michaluk <michal.michaluk@bottega.com.pl>
2023-10-12 15:20:24 +0200

    introduction of Demands and DailyDemand

---
+++ production-impl/src/main/java/shortages/Demands.java
@@ -0,0 +1,63 @@
+package shortages;
+
+import entities.DemandEntity;
+import enums.DeliverySchema;
+import tools.Util;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Demands {
+    private final Map<LocalDate, DemandEntity> demandsPerDay;
+
+    public Demands(List<DemandEntity> demands) {
+        Map<LocalDate, DemandEntity> demandsPerDay = new HashMap<>();
+        for (DemandEntity demand : demands) {
+            demandsPerDay.put(demand.getDay(), demand);
+        }
+        this.demandsPerDay = Collections.unmodifiableMap(demandsPerDay);
+    }
+
+    public DailyDemand get(LocalDate day) {
+        if (demandsPerDay.containsKey(day)) {
+            return new DailyDemand(demandsPerDay.get(day));
+        } else {
+            return null;
+        }
+    }
+
+    public static class DailyDemand {
+        private final DemandEntity demand;
+
+        public DailyDemand(DemandEntity demand) {
+            this.demand = demand;
+        }
+
+        public boolean hasDeliverySchema(DeliverySchema schema) {
+            return Util.getDeliverySchema(demand) == schema;
+        }
+
+        public long getLevel() {
+            return Util.getLevel(demand);
+        }
+
+        public long calculateLevelOnDelivery(long level, long produced) {
+            long levelOnDelivery;
+            if (hasDeliverySchema(DeliverySchema.atDayStart)) {
+                levelOnDelivery = level - getLevel();
+            } else if (hasDeliverySchema(DeliverySchema.tillEndOfDay)) {
+                levelOnDelivery = level - getLevel() + produced;
+            } else if (hasDeliverySchema(DeliverySchema.every3hours)) {
+                // TODO WTF ?? we need to rewrite that app :/
+                throw new UnsupportedOperationException();
+            } else {
+                // TODO implement other variants
+                throw new UnsupportedOperationException();
+            }
+            return levelOnDelivery;
+        }
+    }
+}
